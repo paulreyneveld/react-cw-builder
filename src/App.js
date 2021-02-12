944e293953818c13af9ceea602ee95ef44e535da
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-11 19:25:30 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Design from './components/Design'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello world</h1>
+    <Design />
+    </>
+    );
+}
+
+export default App;
---
+++ src/components/Design.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Design = () => {
+    return (
+        <h1>Hello from Design</h1>
+    )
+}
+
+export default Design
